d21f2e6555402c3f0186af5efd8c46e7452ed602
rngallo <rngallo@ncsu.edu>
2024-08-30 15:50:24 -0400

    C files implemented

--- script.js
+++ script.js
@@ -1,20 +1,112 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Define your projects here with their files
     const projects = [
-        // Define your projects here with their files
+
+        // Java Projects
+        
+
+        // C Projects
         {
-            name: "Project 1",
+            name: "C - P2: List Formatting",
             files: [
                 {
-                    name: "File 1",
-                    path: "projects/project1/file1.c"
+                    name: "formatter.c",
+                    path: "projects/P2/formatter.c"
                 },
                 {
-                    name: "File 2",
-                    path: "projects/project1/file2.java"
+                    name: "date.c",
+                    path: "projects/P2/date.c"
+                },
+                {
+                    name: "date.h",
+                    path: "projects/P2/date.h"
+                },
+                {
+                    name: "name.c",
+                    path: "projects/P2/name.c"
+                },
+                {
+                    name: "name.h",
+                    path: "projects/P2/name.h"
+                },
+                {
+                    name: "ssn.c",
+                    path: "projects/P2/ssn.c"
+                },
+                {
+                    name: "ssn.h",
+                    path: "projects/P2/ssn.h"
+                },
+                {
+                    name: "util.c",
+                    path: "projects/P2/util.c"
+                },
+                {
+                    name: "util.h",
+                    path: "projects/P2/util.h"
                 }
             ]
         },
-        // Add more projects here
+        {
+            name: "C - P3: Stocks",
+            files: [
+                {
+                    name: "trader.c",
+                    path: "projects/P3/trader.c"
+                },
+                {
+                    name: "transaction.c",
+                    path: "projects/P3/transaction.c"
+                },
+                {
+                    name: "transaction.h",
+                    path: "projects/P3/transaction.h"
+                },
+                {
+                    name: "account.c",
+                    path: "projects/P3/account.c"
+                },
+                {
+                    name: "account.h",
+                    path: "projects/P3/account.h"
+                },
+                {
+                    name: "util.c",
+                    path: "projects/P3/util.c"
+                },
+                {
+                    name: "util.h",
+                    path: "projects/P3/util.h"
+                }
+            ]
+        },
+        {
+            name: "C - P4: Trip",
+            files: [
+                {
+                    name: "parks.c",
+                    path: "projects/P4/parks.c"
+                },
+                {
+                    name: "catalog.c",
+                    path: "projects/P4/catalog.c"
+                },
+                {
+                    name: "catalog.h",
+                    path: "projects/P4/catalog.h"
+                }
+            ]
+        },
+        {
+            name: "C - HW3.2: Max Palindrome",
+            files: [
+                {
+                    name: "maxpalindrome-thread.c",
+                    path: "projects/HW3.2/maxpalindrome-thread.c"
+                }
+            ]
+        }
     ];
 
     const projectsContainer = document.getElementById('projects');
